6748f109b82f4ba0373ebaecf4e860578a05cb47
Allan Gonçalves <allangoncalves@outlook.com>
2018-12-04 17:38:20 +0000

    com wsgi e sem flask-migrate

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config(object):
     # ...
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:rimor@2018@localhost:5432/citizenhero'
+    SQLALCHEMY_DATABASE_URI = "mysql://allangoncalves:rimor@2018@allangoncalves.mysql.pythonanywhere-services.com/allangoncalves$citizenhero"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
